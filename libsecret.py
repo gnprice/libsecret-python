5a3164ee0e552550d1627123fa65c01ccf0e8917
Greg Price <gnprice@gmail.com>
2018-11-28 23:39:43 -0800

    Implement items.
    Also switch to staticmethod because I don't want to deal
    just now with generalizing this hashcons cache.

--- libsecret.py
+++ libsecret.py
@@ -1,6 +1,6 @@
 import re
 import sys
-from typing import List, Optional, Type, TypeVar
+from typing import List, Optional, Tuple, Type, TypeVar
 
 from pydbus import SessionBus
 
@@ -34,17 +34,57 @@ class Collection:
     def __init__(self, name: str) -> None:
         self.name = name
 
-    @classmethod
-    def by_path(cls: Type[T_Collection], path: str) -> T_Collection:
-        return cls(strip_prefix('collection/', path))
+    # Keyed by name.
+    __cache = {}  # Dict[str, Collection]
 
-    @classmethod
-    def by_alias(cls: Type[T_Collection], alias: str) -> Optional[T_Collection]:
-        return cls.by_path(proxy().ReadAlias(alias))
+    @staticmethod
+    def get(name: str) -> 'Collection':
+        if name not in Collection.__cache:
+            Collection.__cache[name] = Collection(name)
+        return Collection.__cache[name]
 
-    @classmethod
-    def list(cls: Type[T_Collection]) -> List[T_Collection]:
-        return [cls.by_path(path) for path in proxy().Collections]
+    @staticmethod
+    def by_path(path: str) -> 'Collection':
+        return Collection.get(strip_prefix('collection/', path))
+
+    @staticmethod
+    def by_alias(alias: str) -> Optional['Collection']:
+        return Collection.by_path(proxy().ReadAlias(alias))
+
+    @staticmethod
+    def list() -> List['Collection']:
+        return [Collection.by_path(path) for path in proxy().Collections]
+
+    def proxy(self):
+        return proxy('collection/{}'.format(self.name))
+
+    @property
+    def items(self) -> List['Item']:
+        return [Item.by_path(path) for path in self.proxy().Items]
+
+
+T_Item = TypeVar('T_Item', bound='Item')
+
+class Item:
+    def __init__(self, collection: Collection, name: str) -> None:
+        self.collection = collection
+        self.name = name
+
+    # Keyed by (collection_name, name).
+    __cache = {}  # Dict[Tuple[str, str], Collection]
+
+    @staticmethod
+    def get(collection_name: str, name: str) -> 'Item':
+        key = (collection_name, name)
+        if key not in Item.__cache:
+            Item.__cache[key] = Item(Collection.get(collection_name),
+                                      name)
+        return Item.__cache[key]
+
+    @staticmethod
+    def by_path(path: str) -> 'Item':
+        collection_name, name = strip_prefix('collection/', path).split('/', 1)
+        return Item.get(collection_name, name)
 
 
 def main(argv: List[str]=None):
@@ -56,6 +96,12 @@ def main(argv: List[str]=None):
     print()
     for alias in ['default', 'session']:
         print('{} -> {}'.format(alias, Collection.by_alias(alias).name))
+    print()
+
+    items = Collection.get('login').items
+    print('{} items; a few:'.format(len(items)))
+    for item in items[:3]:
+        print('  {}'.format(item.name))
 
 
 if __name__ == '__main__':
