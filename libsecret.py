4a8d51c90a0866285107c190ea0ac6a378b6a311
Greg Price <gnprice@gmail.com>
2018-11-28 23:08:02 -0800

    Switch to pydbus (a more Pythonic wrapper around the same thing)

--- libsecret.py
+++ libsecret.py
@@ -1,34 +1,41 @@
+import re
 import sys
 from typing import Any, List, Optional
 
-import gi
-gi.require_version('Secret', '1')
-from gi.repository import Secret
+from pydbus import SessionBus
 
 # Important docs:
+#   https://github.com/LEW21/pydbus/blob/master/doc/tutorial.rst#accessing-exported-objects
 #   https://specifications.freedesktop.org/secret-service/
+#
+# Maybe in a pinch:
 #   https://lazka.github.io/pgi-docs/Secret-1/classes/Collection.html
 
-Collection = Any
+bus = SessionBus()
 
-def get_service():
-    return Secret.Service.get_sync(Secret.ServiceFlags.NONE)
+def proxy_object(path=None):
+    return bus.get('.secrets',  # aka org.freedesktop.secrets
+                   path)
 
-def get_collections() -> List[Collection]:
-    s = get_service()
-    s.load_collections_sync()
-    return s.get_collections()
+def shorten_collection_name(path: str) -> str:
+    return re.sub('^/org/freedesktop/secrets/collection/', '', path)
 
-def collection_for_alias(name: str) -> Optional[Collection]:
-    return Secret.Collection.for_alias_sync(
-        get_service(), name, Secret.CollectionFlags.NONE)
+def get_collections() -> List[str]:
+    return [shorten_collection_name(path)
+            for path in proxy_object().Collections]
+
+def get_collection_by_alias(name: str) -> str:
+    return shorten_collection_name(proxy_object().ReadAlias(name))
 
 def main(argv: List[str]=None):
     if argv is None:
         argv = sys.argv[1:]
 
     for c in get_collections():
-        print(repr(c.get_label()))
+        print(c)
+    print()
+    for alias in ['default', 'session']:
+        print('{} -> {}'.format(alias, get_collection_by_alias(alias)))
 
 if __name__ == '__main__':
     sys.exit(main())
