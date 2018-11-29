25bbb09fa91fb52e09e74cb7c41873137aac0bd2
Greg Price <gnprice@gmail.com>
2018-11-28 23:19:48 -0800

    Make in turn a nice typed, object-oriented interface over pydbus.

--- libsecret.py
+++ libsecret.py
@@ -1,8 +1,9 @@
 import re
 import sys
-from typing import Any, List, Optional
+from typing import List, Optional, Type, TypeVar
 
 from pydbus import SessionBus
+
 
 # Important docs:
 #   https://github.com/LEW21/pydbus/blob/master/doc/tutorial.rst#accessing-exported-objects
@@ -11,31 +12,51 @@ from pydbus import SessionBus
 # Maybe in a pinch:
 #   https://lazka.github.io/pgi-docs/Secret-1/classes/Collection.html
 
+
 bus = SessionBus()
 
-def proxy_object(path=None):
+
+def proxy(path=None):
     return bus.get('.secrets',  # aka org.freedesktop.secrets
                    path)
 
-def shorten_collection_name(path: str) -> str:
-    return re.sub('^/org/freedesktop/secrets/collection/', '', path)
 
-def get_collections() -> List[str]:
-    return [shorten_collection_name(path)
-            for path in proxy_object().Collections]
+def strip_prefix(subpath: str, path: str) -> str:
+    prefix = '/org/freedesktop/secrets/' + subpath
+    if not path.startswith(prefix):
+        raise RuntimeError('unexpected path: {}'.format(path))
+    return path[len(prefix):]
 
-def get_collection_by_alias(name: str) -> str:
-    return shorten_collection_name(proxy_object().ReadAlias(name))
+
+T_Collection = TypeVar('T_Collection', bound='Collection')
+
+class Collection:
+    def __init__(self, name: str) -> None:
+        self.name = name
+
+    @classmethod
+    def by_path(cls: Type[T_Collection], path: str) -> T_Collection:
+        return cls(strip_prefix('collection/', path))
+
+    @classmethod
+    def by_alias(cls: Type[T_Collection], alias: str) -> Optional[T_Collection]:
+        return cls.by_path(proxy().ReadAlias(alias))
+
+    @classmethod
+    def list(cls: Type[T_Collection]) -> List[T_Collection]:
+        return [cls.by_path(path) for path in proxy().Collections]
+
 
 def main(argv: List[str]=None):
     if argv is None:
         argv = sys.argv[1:]
 
-    for c in get_collections():
-        print(c)
+    for c in Collection.list():
+        print(c.name)
     print()
     for alias in ['default', 'session']:
-        print('{} -> {}'.format(alias, get_collection_by_alias(alias)))
+        print('{} -> {}'.format(alias, Collection.by_alias(alias).name))
+
 
 if __name__ == '__main__':
     sys.exit(main())
