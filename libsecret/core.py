a9fcef08ad689acf9445a868fbbca733e41a236d
Greg Price <gnprice@gmail.com>
2018-11-29 23:41:45 -0800

    Add create collection; requiring prompts, requiring signals.

--- libsecret/core.py
+++ libsecret/core.py
@@ -1,7 +1,10 @@
 import re
-from typing import List, Optional, Tuple, Type, TypeVar
+from typing import Any, Callable, List, Optional, Tuple, Type, TypeVar
 
-from pydbus import SessionBus
+from gi.repository import GLib
+from pydbus import SessionBus, Variant
+
+from .windowid import active_window_id
 
 
 # Important handy docs on the upstream API are linked in NOTES.
@@ -12,7 +15,9 @@ from pydbus import SessionBus
 bus = SessionBus()
 
 
-def proxy(path=None):
+ProxyT = Any
+
+def proxy(path: Optional[str]=None) -> ProxyT:
     return bus.get(
         '.secrets',  # aka org.freedesktop.secrets
         path)        # aka /org/freedesktop/secrets${path:+/$path}
@@ -23,6 +28,34 @@ def strip_prefix(subpath: str, path: str) -> str:
     if not path.startswith(prefix):
         raise RuntimeError('unexpected path: {}'.format(path))
     return path[len(prefix):]
+
+
+_main_loop = GLib.MainLoop()
+
+def rpc(path: str, invoke: Callable[[ProxyT], None]) -> Tuple:
+    result = [None]
+    def dbus_cb(_, _1, _2, _3, params) -> None:
+        _main_loop.quit()
+        result[0] = params
+    with bus.subscribe(sender='org.freedesktop.secrets', object=path,
+                       # TODO might be good to pin down the other attributes
+                       signal_fired=dbus_cb):
+        invoke(proxy(path))
+        _main_loop.run()
+    return result[0]
+
+
+class Prompt:
+    def __init__(self, path: str) -> None:
+        self.path = path
+
+    @staticmethod
+    def complete(prompt_path: str) -> str:
+        window_id = active_window_id()
+        dismissed, result = rpc(prompt_path, lambda p: p.Prompt(window_id))
+        if dismissed:
+            raise RuntimeError('Prompt dismissed')
+        return result
 
 
 T_Collection = TypeVar('T_Collection', bound='Collection')
@@ -51,6 +84,15 @@ class Collection:
     @staticmethod
     def list() -> List['Collection']:
         return [Collection.by_path(path) for path in proxy().Collections]
+
+    @staticmethod
+    def create(label: str, alias: Optional[str]=None) -> str:
+        properties = {'org.freedesktop.Secret.Collection.Label':
+                      Variant.new_string(label)}
+        result, prompt_path = proxy().CreateCollection(properties, alias or '')
+        if result != '/':
+            return result
+        return Prompt.complete(prompt_path)
 
     def proxy(self):
         return proxy('collection/{}'.format(self.name))
